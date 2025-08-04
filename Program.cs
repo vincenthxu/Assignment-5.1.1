a2819100d61ed1eea5a82085e71cc8d8a65bef44
Vincent Xu <vincent.xu1@outlook.com>
2025-08-04 12:55:28 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Assignment_5._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            if (args.Length > 0)
+            {
+                int num;
+                foreach (var arg in args)
+                {
+                    num = Convert.ToInt32(arg);
+                    Console.WriteLine($"{num} : {isPalindrome(num)}");
+                }
+            }
+        }
+        static bool isPalindrome(int num)
+        {
+            if (num < 0)
+            {
+                return false;
+            }
+
+            return num == IntReverse(num);
+        }
+
+        static int IntReverse(int num)
+        {
+            int reverse = 0;
+            while(num > 0)
+            {
+                reverse *= 10;
+                reverse += num % 10;
+                num /= 10;
+            }
+            return reverse;
+        }
+    }
+}
